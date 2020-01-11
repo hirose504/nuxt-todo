7ab65c77f31c06140d885d3fd7dc763fcc9b7195
hirose504 <hirose504@yahoo.co.jp>
2020-01-12 03:11:30 +0900

    add index.vue for typescript

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    {{ message }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+@Component
+export default class PageIndex extends Vue {
+  message: string = 'This is a message'
+}
+</script>
